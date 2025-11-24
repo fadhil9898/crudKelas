1c0baa42ba99fba2f2288076027c0192e0aa5504
moch_fadhlil <fadhilahfadhil770@gmail.com>
2025-11-24 12:19:20 +0700

    fungtion edit data

--- fungsi.php
+++ fungsi.php
@@ -19,4 +19,39 @@
 
             return true;
     }
+    function edit_data($data, $files){
+        $id_mahasiswa = $data['id_mahasiswa'];
+            $nim = $data['nim'];
+            $nama_mahasiswa = $data['nama'];
+            $jenis_kelamin = $data['jenis_kelamin'];
+            $alamat = $data['alamat'];
+
+            $queryShow = "SELECT * FROM mahasiswa WHERE id_mahasiswa = '$id_mahasiswa';";
+            $sqlShow = mysqli_query($GLOBALS['conn'], $queryShow);
+            $result = mysqli_fetch_assoc($sqlShow);
+
+            if($files['foto']['name'] == ""){     
+                $foto = $result['foto_mahasiswa'];
+            }else {
+                $foto = $files['foto']['name'];
+                unlink('img/'.$result['foto_mahasiswa']);
+                move_uploaded_file($files['foto']['tmp_name'],'img/'.$files['foto']['name']);
+            }
+
+            $query = "UPDATE mahasiswa SET nim='$nim', nama_mahasiswa='$nama_mahasiswa', jenis_kelamin='$jenis_kelamin',
+                        alamat='$alamat', foto_mahasiswa='$foto' WHERE id_mahasiswa='$id_mahasiswa';";
+
+            $sql = mysqli_query($GLOBALS['conn'], $query);
+
+            return true;
+    }
+    // function hapus_data($data){
+    //     $id_mahasiswa = $data['hapus'];
+    //     $queryShow = "SELECT * FROM mahasiswa WHERE id_mahasiswa = '$id_mahasiswa';";
+    //     $sqlShow = mysqli_query($GLOBALS['conn'], $queryShow);
+    //     $result = mysqli_fetch_assoc($sqlShow);
+    //     unlink("img/".$result['foto_mahasiswa']);
+    //     $query = "DELETE FROM mahasiswa WHERE id_mahasiswa = '$id_mahasiswa'";
+    //     $sql = mysqli_query($GLOBALS['conn'], $query);
+    // }
 ?>
--- proses.php
+++ proses.php
@@ -13,51 +13,22 @@ include 'fungsi.php';
             }
 
         }elseif ($_POST['aksi'] == "edit"){
-            // var_dump ($_POST);
-            echo "edit data <a href='index.php'>[Home] <a>";
-
-            $id_mahasiswa = $_POST['id_mahasiswa'];
-            $nim = $_POST['nim'];
-            $nama_mahasiswa = $_POST['nama'];
-            $jenis_kelamin = $_POST['jenis_kelamin'];
-            $alamat = $_POST['alamat'];
-
-            $queryShow = "SELECT * FROM mahasiswa WHERE id_mahasiswa = '$id_mahasiswa';";
-            $sqlShow = mysqli_query($conn, $queryShow);
-            $result = mysqli_fetch_assoc($sqlShow);
-
-            if($_FILES['foto']['name'] == ""){     
-                $foto = $result['foto_mahasiswa'];
+            $berhasil = edit_data($_POST, $_FILES);
+            if($berhasil){
+                header("location: index.php");
             }else {
-                $foto = $_FILES['foto']['name'];
-                unlink('img/'.$result['foto_mahasiswa']);
-                move_uploaded_file($_FILES['foto']['tmp_name'],'img/'.$_FILES['foto']['name']);
+                echo $berhasil;
             }
-
-            $query = "UPDATE mahasiswa SET nim='$nim', nama_mahasiswa='$nama_mahasiswa', jenis_kelamin='$jenis_kelamin',
-                        alamat='$alamat', foto_mahasiswa='$foto' WHERE id_mahasiswa='$id_mahasiswa';";
-
-            $sql = mysqli_query($conn, $query);
-            header("location: index.php");
-
         }
     }
-    if(isset($_GET['hapus'])){
-        $id_mahasiswa = $_GET['hapus'];
-
-        $queryShow = "SELECT * FROM mahasiswa WHERE id_mahasiswa = '$id_mahasiswa';";
-        $sqlShow = mysqli_query($conn, $queryShow);
-        $result = mysqli_fetch_assoc($sqlShow);
-        unlink("img/".$result['foto_mahasiswa']);
-
-        $query = "DELETE FROM mahasiswa WHERE id_mahasiswa = '$id_mahasiswa'";
-        $sql = mysqli_query($conn, $query);
-        if($sql){
-                header("location: index.php");
-                // echo "berhasil <a href='index.php'>[Home] <a>";
-            }else {
-                echo $query;
-            }   
-            // echo "hapus data <a href='index.php'>[Home] <a>";
-        }
+    // if(isset($_GET['hapus'])){
+    //     hapus_data($_GET);
+    //     if($sql){
+    //             header("location: index.php");
+    //             // echo "berhasil <a href='index.php'>[Home] <a>";
+    //         }else {
+    //             echo $query;
+    //         }   
+    //         // echo "hapus data <a href='index.php'>[Home] <a>";
+    //     }
 ?>
